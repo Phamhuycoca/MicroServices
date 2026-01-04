951a94cdefcd8b1a59a83c5bddda8541d59bb571
Phamhuycoca <phamkhachuy240702@gmail.com>
2026-01-04 21:27:34 +0700

    setup pretter

--- Frontend/Frontend/eslint.config.js
+++ Frontend/Frontend/eslint.config.js
@@ -4,10 +4,12 @@ import reactHooks from 'eslint-plugin-react-hooks'
 import reactRefresh from 'eslint-plugin-react-refresh'
 import tseslint from 'typescript-eslint'
 import { defineConfig, globalIgnores } from 'eslint/config'
+import eslintPluginPrettier from 'eslint-plugin-prettier'
 
 export default defineConfig([
   globalIgnores(['dist']),
   {
+    ignores: ['vite.config.ts'],
     files: ['**/*.{ts,tsx}'],
     extends: [
       js.configs.recommended,
@@ -15,6 +17,25 @@ export default defineConfig([
       reactHooks.configs.flat.recommended,
       reactRefresh.configs.vite,
     ],
+    plugins:{
+      prettier: eslintPluginPrettier
+    },
+    rules:{
+      'prettier/prettier': [
+            'warn',
+            {
+              arrowParens: 'always',
+              semi: false,
+              trailingComma: 'none',
+              tabWidth: 2,
+              endOfLine: 'auto',
+              useTabs: false,
+              singleQuote: true,
+              printWidth: 120,
+              jsxSingleQuote: true
+            }
+          ]
+    },
     languageOptions: {
       ecmaVersion: 2020,
       globals: globals.browser,
--- Frontend/Frontend/src/App.tsx
+++ Frontend/Frontend/src/App.tsx
@@ -1,5 +1,3 @@
-export const App=()=>{
-  return (
-    <h1>Okkkk</h1>
-  )
+export const App = () => {
+  return <h1>Okkkk</h1>
 }
